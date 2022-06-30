3dd1307902b9ba082227494082bb8bdb3d4c914e
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2022-06-30 17:48:50 -0300

    FirstAid

---
+++ Assets/Scripts/FirstAid.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAid : MonoBehaviour
+{
+    [SerializeField]
+    private int _healValue;
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        if(player)
+        {
+            player.TakeDamage(-_healValue);
+            Destroy(gameObject);
+        } 
+    }
+
+    private void Heal()
+    {
+        
+    }
+}
