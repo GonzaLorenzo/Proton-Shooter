b18a815b6f6e10bdc7adc1a81479d62144e26bbd
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2022-07-01 13:00:01 -0300

    Non-Auth Finished

--- Assets/Scripts/FirstAid.cs
+++ Assets/Scripts/FirstAid.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class FirstAid : MonoBehaviour
+public class FirstAid : MonoBehaviourPun
 {
+    [SerializeField]
+    Player _playerToHeal;
     [SerializeField]
     private int _healValue;
     void OnTriggerEnter(Collider other)
@@ -11,13 +14,16 @@ public class FirstAid : MonoBehaviour
         var player = other.GetComponent<Player>();
         if(player)
         {
-            player.TakeDamage(-_healValue);
-            Destroy(gameObject);
+            _playerToHeal = other.GetComponent<Player>(); 
+            photonView.RPC("RPC_DestroyThisGO", RpcTarget.AllBuffered);
+            //Destroy(gameObject);
         } 
     }
 
-    private void Heal()
+    [PunRPC]
+    void RPC_DestroyThisGO()
     {
-        
+        _playerToHeal.TakeDamage(-_healValue);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/LightsOut.cs
+++ Assets/Scripts/LightsOut.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class LightsOut : MonoBehaviour
+public class LightsOut : MonoBehaviourPun
 {
     [SerializeField]
     private Light _mySpotlight;
     void OnTriggerEnter(Collider other)
     {
+        photonView.RPC("RPC_DestroyThisLight", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void RPC_DestroyThisLight()
+    {
         _mySpotlight.enabled = false;
-        Destroy(this);   
+        Destroy(this);  
     }
 }
