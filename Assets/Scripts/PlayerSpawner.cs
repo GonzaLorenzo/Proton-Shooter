77f1a28c8014a630f77d232dc80303fb7acebf9b
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2022-06-30 21:49:29 -0300

    PlayerSpawner Update

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -9,9 +9,22 @@ public class PlayerSpawner : MonoBehaviour
     private GameObject _playerPrefab;
     [SerializeField]
     private Transform _spawnPos;
+    [SerializeField]
+    private Transform _spawnPos2;
+    private int _spawnNumber = 1;
 
     private void Start()
     {
-        PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPos.position, Quaternion.identity);
+        switch(_spawnNumber)
+        {
+        case 1:
+            PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPos.position, Quaternion.identity);
+            _spawnNumber ++;
+                break;
+        case 2:
+            PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPos2.position, Quaternion.identity);
+            break;
+        }
+        
     }
 }
